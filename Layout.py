bb805032e6325d904dcb121705e4cac46d85f05b
Severshe <S.Eversheim@Hotmail.de>
2015-04-21 13:57:06 +0200

    Update Layout.py

--- Layout.py
+++ Layout.py
@@ -1,18 +1,29 @@
+# TEST TEST TEST
+
 import os
 from os.path import join, getsize
-import pyglet
-from pytag import Audio
+
 from tkinter import *
 from tkinter import ttk
+
+import pyglet
+from pyglet.window import key   #Keyboard und Maus Unterstuetzung
+
+#from pytag import Audio
+
+import math
+
 import threading
 
-#path = "F:\\Downloads\MarSve-Player\mp3"
-path = "/Users/Luftikus/Desktop/mp3"
-
+#path = "K:\Musik"
+path = "F:\Downloads\MarSve-Player\mp3"
+#path = "/Users/Luftikus/Desktop/mp3"
+ 
 root = Tk()
 root.title("Mp3 Player MarSve")
 
 list_loc =[] #Liste fuer die Eigentlichen Item Daten der Playlist
+currenttrack_id = 0
 
 def addToList():
     playlist.insert(END, os.path.basename(manlist.focus()))
@@ -23,16 +34,38 @@ def delFromList():
     idx = int(idxs[0])
     playlist.delete(idx)
     list_loc.pop(idx)
+    
+def nexttrack():
+    global currenttrack_id
+    currenttrack_id = currenttrack_id + 1
+    player.next()
+    bar["maximum"] = player.source.duration
 
+def prevtrack():
+    global currenttrack_id
+    currenttrack_id = currenttrack_id - 1
+    idx = currenttrack_id
+    player.delete()
+    for i in range(idx, len(list_loc)):
+        music = pyglet.media.load(list_loc[i])
+        player.queue(music)
+    bar["maximum"] = player.source.duration
+    player.play()
+    update_clock()
+    
 player = pyglet.media.Player()
 
 #############################################################################
 #Progressbar Update
 #############################################################################
 def update_clock():
+    if bar["maximum"] != player.source.duration:
+        global currenttrack_id
+        currenttrack_id = currenttrack_id + 1
+        bar["maximum"] = player.source.duration
+        print('New ID',currenttrack_id)
     if player.playing:
         threading.Timer(0.25, update_clock).start()
-        bar["maximum"] = player.source.duration
         bar["value"] = player.time
 #############################################################################
 #Progressbar Update
@@ -59,13 +92,15 @@ def play_pause():
 def playlist_play(event):
     idxs = playlist.curselection()
     idx = int(idxs[0])
+    global currenttrack_id
+    currenttrack_id = idx
     player.delete()
     for i in range(idx, len(list_loc)):
         music = pyglet.media.load(list_loc[i])
         player.queue(music)
+    bar["maximum"] = player.source.duration
     player.play()
     update_clock()
-    return music
 #############################################################################
 #Playlist Doubleclick
 #############################################################################
@@ -100,20 +135,21 @@ manlist.configure(yscrollcommand=manlistscroll.set)
 songtext = ttk.Label(playframe, text="Artist - Song")
 songtext.grid(column=1, row=1, columnspan=6, sticky=(N, W, E))
 
-prevbutton = ttk.Button(playframe, text='Prev')
+prevbutton = ttk.Button(playframe, text='Prev', command=prevtrack)
 prevbutton.grid(column=1, row=2, sticky=(N, W, E, S))
 
 playbutton = ttk.Button(playframe, text='Play', command=play_pause)
 playbutton.grid(column=2, row=2, sticky=(N, W, E, S))
 
-nextbutton =ttk.Button(playframe, text='Next')
-nextbutton.grid(column=3, row=2, sticky=(N, W, E, S))
+bar =ttk.Progressbar(playframe, orient=HORIZONTAL, length=200, mode='determinate')
+bar.grid(column=1, row=3, columnspan=3, sticky=(N, W, E, S))
 
-progressbar =ttk.Progressbar(playframe, orient=HORIZONTAL, length=200, mode='determinate')
-progressbar.grid(column=1, row=3, columnspan=3, sticky=(N, W, E, S))
+nextbutton = ttk.Button(playframe, text='Next', command=nexttrack)
+nextbutton.grid(column=3, row=2, sticky=(N, W, E, S))
 
 playlist = Listbox(playframe)
 playlist.grid(column=1, row=4, columnspan=3, sticky=(N, W, E, S))
+playlist.bind('<Double-Button-1>', playlist_play)
 
 manlistscroll = ttk.Scrollbar( playframe, orient=VERTICAL, command=playlist.yview)
 manlistscroll.grid(column=3, row=4, sticky=(N, E, S))
@@ -131,11 +167,12 @@ entbutton.grid(column=3, row=5, sticky=(E, S))
 #############################################################################
 #Direct Play
 #############################################################################
-def onClick(event):
+def direkt_play(event):
     player.delete()
     music = pyglet.media.load(manlist.focus())
     player.queue(music)
     player.play()
+    source = player.source
     update_clock()
     return music
 #############################################################################
@@ -157,30 +194,18 @@ def scanPath(verz):
             for k in range(0,len(path[j])):
                 if i==0:
                     manlist.insert("", 'end', os.path.join(path[0], path[j][k]), text=path[j][k], tags='Play')
-                    manlist.tag_bind('Play', '<Double-Button-1>', onClick)
+                    manlist.tag_bind('Play', '<Double-Button-1>', direkt_play)
                 else:
                     manlist.insert(path[0], 'end', os.path.join(path[0], path[j][k]), text=path[j][k], tags='Play')
-                    manlist.tag_bind('Play', '<Double-Button-1>', onClick)
+                    manlist.tag_bind('Play', '<Double-Button-1>', direkt_play)
                 k += 1
             j += 1
         i += 1
-
-## Hier nochmal die Schleife die ich gefunden hatte:
-#def scanPath(verz):
-#    for name in os.listdir(verz):
-#        pfad = os.path.join(verz, name)
-#
-#        if os.path.isfile(pfad):
-#            manlist.insert("", 'end', name, text=name)
-#        else:
-#            scanPath(pfad)
-#
-scanPath(path)
-
 #############################################################################
 #Dateimanager
 #############################################################################
 
-for child in mainframe.winfo_children(): child.grid_configure(padx=2, pady=2)
+scanPath(path)
 
+for child in mainframe.winfo_children(): child.grid_configure(padx=2, pady=2)
 root.mainloop()
